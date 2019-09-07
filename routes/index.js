eee2a318cdece1524f956892a045d9d3b2a2ef43
santiago cuello <santiago.cuello@inthegra.com.ar>
2019-09-07 17:03:36 -0300

    Se finaliza la primera pantalla de la consula de los datos de la persona

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,11 @@ var requiresLogin = require("./requiresLogin");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
 
 /* GET login */
-router.get('/login', function(req, res, next){
-  res.render('login', {title: "Login"});
+router.get('/login', function(req, res, next) {
+    res.render('login', { title: "Login" });
 });
 module.exports = router;
